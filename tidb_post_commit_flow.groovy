47ca261dd43dddae0dc45964e67ca0ea8d743cc4
iamxy <kenneth.liu.xy@gmail.com>
2017-01-08 21:10:30 +0800

    test: #1

---
+++ tidb_post_commit_flow.groovy
@@ -0,0 +1,41 @@
+#!groovy
+
+node {
+    def workspace = pwd()
+
+    catchError {
+        stage('Checkout') {
+            dir("bank-test"){
+                git url: 'https://github.com/iamxy/jenkins-ci.git'
+            }
+
+            stash includes: 'bank-test/**', name: 'source-pingcap'
+        }
+
+        stage('Test') {
+            def branches = [:]
+
+            branches["Unit Test"] = {
+                echo "workspace: ${workspace}"
+                dir("output") {
+                    unstash "source-pingcap"
+                }
+            }
+
+            parallel branches
+        }
+
+        currentBuild.result = "SUCCESS"
+    }
+
+    def changeLogText = ""
+    for (int i = 0; i < currentBuild.changeSets.size(); i++) {
+        for (int j = 0; j < currentBuild.changeSets[i].items.length; j++) {
+            def commitId = "${currentBuild.changeSets[i].items[j].commitId}"
+            def commitMsg = "${currentBuild.changeSets[i].items[j].msg}"
+            changeLogText += "\n" + commitId.take(7) + " " + commitMsg
+        }
+    }
+
+    echo "${changeLogText}"
+}
